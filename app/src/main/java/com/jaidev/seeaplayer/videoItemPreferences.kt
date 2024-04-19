d2d03041aa3b832b2f030fc67822ac687bb2c3d0
jai sharma <sussesstime4224.in>
2024-04-19 17:14:08 +0530

    5 solved error for now

---
+++ app/src/main/java/com/jaidev/seeaplayer/videoItemPreferences.kt
@@ -0,0 +1,22 @@
+package com.jaidev.seeaplayer
+
+import android.content.Context
+
+class videoItemPreferences(private val context: Context) {
+
+//    private val prefs: SharedPreferences by lazy {
+//        context.getSharedPreferences("file_item_prefs", Context.MODE_PRIVATE)
+//    }
+//
+//    // Save updated FileItem to SharedPreferences
+//    fun saveFileItem(videoData: VideoData) {
+//        val key = VideoData.getPrefKey(videoData)
+//        prefs.edit().putString(key, videoData.originalFileName).apply()
+//    }
+//
+//    // Retrieve FileItem's originalFileName from SharedPreferences
+//    fun getOriginalFileName(fileItem: VideoData): String {
+//        val key = VideoData.getPrefKey(fileItem)
+//        return prefs.getString(key, fileItem.originalFileName) ?: fileItem.originalFileName
+//    }
+}
