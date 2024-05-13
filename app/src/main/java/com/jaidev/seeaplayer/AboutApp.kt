1410a97592639b4c35e80c2cdd4340cec4b0aec8
jai sharma <sussesstime4224.in>
2024-05-13 15:47:23 +0530

    last app thinks

---
+++ app/src/main/java/com/jaidev/seeaplayer/AboutApp.kt
@@ -0,0 +1,145 @@
+package com.jaidev.seeaplayer
+
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
+import com.jaidev.seeaplayer.databinding.ActivityAboutAppBinding
+
+class AboutApp : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutAppBinding
+    private lateinit var swipeRefreshLayout: LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About SeeA Player"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        setActionBarGradient()
+        swipeRefreshLayout = binding.aboutAppActivity
+        // Set the background color of SwipeRefreshLayout based on app theme
+        setSwipeRefreshBackgroundColor()
+
+        // Find TextViews by their IDs
+        val textView1: TextView = findViewById(R.id.about_para_1)
+        val textView2: TextView = findViewById(R.id.about_para_2)
+        val textView3: TextView = findViewById(R.id.about_para_3)
+        val textView4: TextView = findViewById(R.id.about_para_4)
+        val textView5: TextView = findViewById(R.id.about_para_5)
+        val textView6: TextView = findViewById(R.id.about_para_6)
+
+
+// Set text style to bold for specific words
+        var aboutPara1 = resources.getString(R.string.about_para_1)
+        aboutPara1 = aboutPara1.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara1 = aboutPara1.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView1.text = HtmlCompat.fromHtml(aboutPara1, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        var aboutPara2 = resources.getString(R.string.about_para_2)
+        aboutPara2 = aboutPara2.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara2 = aboutPara2.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView2.text = HtmlCompat.fromHtml(aboutPara2, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        var aboutPara3 = resources.getString(R.string.about_para_3)
+        aboutPara3 = aboutPara3.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara3 = aboutPara3.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView3.text = HtmlCompat.fromHtml(aboutPara3, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        var aboutPara4 = resources.getString(R.string.about_para_4)
+        aboutPara4 = aboutPara4.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara4 = aboutPara4.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView4.text = HtmlCompat.fromHtml(aboutPara4, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        var aboutPara5 = resources.getString(R.string.about_para_5)
+        aboutPara5 = aboutPara5.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara5 = aboutPara5.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView5.text = HtmlCompat.fromHtml(aboutPara5, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        var aboutPara6 = resources.getString(R.string.about_para_6)
+        aboutPara6 = aboutPara6.replace("SeeA Player", "<b>SeeA Player</b>")
+        aboutPara6 = aboutPara6.replace("WhiteMarballs Group", "<b>WhiteMarballs Group</b>")
+        textView6.text = HtmlCompat.fromHtml(aboutPara6, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+// Add similar lines for other TextViews
+
+
+
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+    private fun setSwipeRefreshBackgroundColor() {
+        val isDarkMode = when (resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK) {
+            android.content.res.Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false
+        }
+
+        if (isDarkMode) {
+            // Dark mode is enabled, set background color to #012030
+            swipeRefreshLayout.setBackgroundColor(resources.getColor(R.color.dark_cool_blue))
+        } else {
+            // Light mode is enabled, set background color to white
+            swipeRefreshLayout.setBackgroundColor(resources.getColor(android.R.color.white))
+        }
+    }
+    private fun setActionBarGradient() {
+        // Check the current night mode
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_NO) {
+            // Light mode is applied
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@AboutApp,
+                        R.drawable.background_actionbar_light
+                    )
+                )
+            }
+        } else if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            // Dark mode is applied
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@AboutApp,
+                        R.drawable.background_actionbar
+                    )
+                )
+            }
+        } else {
+            // System Default mode is applied
+            val isSystemDefaultDarkMode = when (resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK) {
+                android.content.res.Configuration.UI_MODE_NIGHT_YES -> true
+                else -> false
+            }
+            // Set the ActionBar color based on the System Default mode
+            if (isSystemDefaultDarkMode) {
+                // System Default mode is dark
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@AboutApp,
+                            R.drawable.background_actionbar
+                        )
+                    )
+                }
+            } else {
+                // System Default mode is light
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@AboutApp,
+                            R.drawable.background_actionbar_light
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jaidev/seeaplayer/FeedBackActivity.kt
@@ -0,0 +1,135 @@
+package com.jaidev.seeaplayer
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import com.jaidev.seeaplayer.databinding.ActivityFeedBackBinding
+
+class FeedBackActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityFeedBackBinding
+    private lateinit var swipeRefreshLayout: LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFeedBackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "Feedback"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        binding.sendFA.setOnClickListener {
+            val feedbackMsg = binding.feedbackMsgFA.text.toString()
+            val subject = binding.topicFA.text.toString()
+            val email = binding.emailFA.text.toString()
+
+            if (feedbackMsg.isNotEmpty() && subject.isNotEmpty()) {
+                val emailIntent = Intent(Intent.ACTION_SENDTO)
+                emailIntent.data = Uri.parse("mailto:")
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("seeaplayer1019@gmail.com"))
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback to SeeA Player: $subject")
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Feedback message: $feedbackMsg\nUser's email: $email")
+                emailIntent.putExtra(Intent.EXTRA_TEXT, """
+                    Hello,
+                    
+                    Topic: $subject
+                    
+                    ${if (email.isNotEmpty()) "User's Email: $email\n" else ""}
+                    
+                    Feedback: $feedbackMsg
+                    
+                    ////////////////////***** SeeA Player Feedback *****////////////////////
+                    
+                    Regards,
+                    SeeA Player
+                """.trimIndent())
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send feedback via..."))
+                } catch (e: Exception) {
+                    Toast.makeText(this, "No email clients installed.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        setActionBarGradient()
+        swipeRefreshLayout = binding.feedBackActivity
+
+        // Set the background color of SwipeRefreshLayout based on app theme
+        setSwipeRefreshBackgroundColor()
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+    private fun setSwipeRefreshBackgroundColor() {
+        val isDarkMode = when (resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK) {
+            android.content.res.Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false
+        }
+
+        if (isDarkMode) {
+            // Dark mode is enabled, set background color to #012030
+            swipeRefreshLayout.setBackgroundColor(resources.getColor(R.color.dark_cool_blue))
+        } else {
+            // Light mode is enabled, set background color to white
+            swipeRefreshLayout.setBackgroundColor(resources.getColor(android.R.color.white))
+        }
+    }
+    private fun setActionBarGradient() {
+        // Check the current night mode
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_NO) {
+            // Light mode is applied
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@FeedBackActivity,
+                        R.drawable.background_actionbar_light
+                    )
+                )
+            }
+        } else if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            // Dark mode is applied
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@FeedBackActivity,
+                        R.drawable.background_actionbar
+                    )
+                )
+            }
+        } else {
+            // System Default mode is applied
+            val isSystemDefaultDarkMode = when (resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK) {
+                android.content.res.Configuration.UI_MODE_NIGHT_YES -> true
+                else -> false
+            }
+            // Set the ActionBar color based on the System Default mode
+            if (isSystemDefaultDarkMode) {
+                // System Default mode is dark
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@FeedBackActivity,
+                            R.drawable.background_actionbar
+                        )
+                    )
+                }
+            } else {
+                // System Default mode is light
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@FeedBackActivity,
+                            R.drawable.background_actionbar_light
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
