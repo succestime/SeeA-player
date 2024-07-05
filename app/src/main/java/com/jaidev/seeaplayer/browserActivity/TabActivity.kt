dfe22fc571ec8b28a3555d45028dbf8df8a53e37
jai sharma <sussesstime4224.in>
2024-07-05 13:55:26 +0530

    last app thinks

---
+++ app/src/main/java/com/jaidev/seeaplayer/browserActivity/TabActivity.kt
@@ -0,0 +1,225 @@
+package com.jaidev.seeaplayer.browserActivity
+
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.jaidev.seeaplayer.R
+import com.jaidev.seeaplayer.allAdapters.TabAdapter
+import com.jaidev.seeaplayer.browseFregment.HomeFragment
+import com.jaidev.seeaplayer.databinding.ActivityTabBinding
+
+class TabActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityTabBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTabBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+            binding.tabsRV.setHasFixedSize(true)
+        binding.tabsRV.layoutManager = GridLayoutManager(this, 2)
+
+        binding.tabsRV.adapter = TabAdapter(this)
+
+        setActionBarGradient()
+        updateEmptyViewVisibility()
+        setupActionBar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tab_more, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.open_more -> {
+                showPopupMenu(findViewById(R.id.open_more))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged", "DiscouragedPrivateApi")
+    private fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        popup.menuInflater.inflate(R.menu.tab_click_more_menu, popup.menu)
+
+        // Check if the RecyclerView is empty
+        if ((binding.tabsRV.adapter as TabAdapter).itemCount == 0) {
+            // Disable and set the color of the menu items
+            val clearAllTabsItem = popup.menu.findItem(R.id.clearAllTabs)
+            val selectTabsItem = popup.menu.findItem(R.id.selectTabs)
+
+            clearAllTabsItem.isEnabled = false
+            selectTabsItem.isEnabled = false
+
+            val grayColor = ContextCompat.getColor(this, R.color.gray) // Replace R.color.gray with your actual gray color resource
+
+            val clearAllTabsTitle = SpannableString(clearAllTabsItem.title)
+            clearAllTabsTitle.setSpan(ForegroundColorSpan(grayColor), 0, clearAllTabsTitle.length, 0)
+            clearAllTabsItem.title = clearAllTabsTitle
+
+            val selectTabsTitle = SpannableString(selectTabsItem.title)
+            selectTabsTitle.setSpan(ForegroundColorSpan(grayColor), 0, selectTabsTitle.length, 0)
+            selectTabsItem.title = selectTabsTitle
+
+            // If you have icons, you can set their color as well
+            if (clearAllTabsItem.icon != null) {
+                clearAllTabsItem.icon!!.mutate().setColorFilter(grayColor, PorterDuff.Mode.SRC_IN)
+            }
+            if (selectTabsItem.icon != null) {
+                selectTabsItem.icon!!.mutate().setColorFilter(grayColor, PorterDuff.Mode.SRC_IN)
+            }
+        }
+        // Use reflection to show icons in the popup menu
+        try {
+            val fieldPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+            fieldPopup.isAccessible = true
+            val mPopup = fieldPopup.get(popup)
+            mPopup.javaClass.getDeclaredMethod("setForceShowIcon", Boolean::class.java).invoke(mPopup, true)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        popup.setOnMenuItemClickListener { menuItem ->
+            if (!menuItem.isEnabled) {
+                // If the item is disabled, do nothing
+                return@setOnMenuItemClickListener true
+            }
+            when (menuItem.itemId) {
+                R.id.newTab -> {
+                    changeTab("Home", HomeFragment())
+                    finish()
+                    true
+                }
+                R.id.clearAllTabs -> {
+                    showClearAllTabsDialog()
+                    true
+                }
+                R.id.selectTabs -> {
+                    // Handle select tabs action
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showClearAllTabsDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_clear_all_tabs, null)
+        val dialog = android.app.AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        dialogView.findViewById<TextView>(R.id.cancelButton).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialogView.findViewById<TextView>(R.id.confirmButton).setOnClickListener {
+            LinkTubeActivity.tabsList.clear()
+            (binding.tabsRV.adapter as TabAdapter).notifyDataSetChanged()
+            updateEmptyViewVisibility()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    fun updateEmptyViewVisibility() {
+        if (LinkTubeActivity.tabsList.isEmpty()) {
+            binding.videoEmptyStateLayout.visibility = View.VISIBLE
+            binding.tabsRV.visibility = View.GONE
+        } else {
+            binding.videoEmptyStateLayout.visibility = View.GONE
+            binding.tabsRV.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateEmptyViewVisibility()
+    }
+    private fun setActionBarGradient() {
+        val nightMode = AppCompatDelegate.getDefaultNightMode()
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_NO) {
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@TabActivity,
+                        R.drawable.background_actionbar_light
+                    )
+                )
+            }
+        } else if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            supportActionBar?.apply {
+                setBackgroundDrawable(
+                    ContextCompat.getDrawable(
+                        this@TabActivity,
+                        R.drawable.background_actionbar
+                    )
+                )
+            }
+        } else {
+            val isSystemDefaultDarkMode = when (resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK) {
+                android.content.res.Configuration.UI_MODE_NIGHT_YES -> true
+                else -> false
+            }
+            if (isSystemDefaultDarkMode) {
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@TabActivity,
+                            R.drawable.background_actionbar
+                        )
+                    )
+                }
+            } else {
+                supportActionBar?.apply {
+                    setBackgroundDrawable(
+                        ContextCompat.getDrawable(
+                            this@TabActivity,
+                            R.drawable.background_actionbar_light
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    private fun setupActionBar() {
+        val inflater = LayoutInflater.from(this)
+        val customActionBarView = inflater.inflate(R.layout.tab_custom_action_bar_layout, null)
+
+        val titleTextView = customActionBarView.findViewById<TextView>(R.id.titleNewTab)
+        titleTextView.text = "New tab"
+
+        val connectivityCardView = customActionBarView.findViewById<LinearLayout>(R.id.connectivityCardView)
+
+        connectivityCardView.setOnClickListener {
+            changeTab("Home", HomeFragment())
+            finish()
+        }
+
+       supportActionBar?.apply {
+            setDisplayShowCustomEnabled(true)
+            setDisplayShowTitleEnabled(false)
+            customView = customActionBarView
+        }
+    }
+
+}
